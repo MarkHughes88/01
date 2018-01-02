f4525a2bcb621e271aa3315430d26fdbd22b7a4f
Mark Hughes <Mark.Hughes@terraquest.co.uk>
2018-01-02 09:15:45 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Menu from './components/menu/menu'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hello
+        <Menu />
+      </div>
+    );
+  }
+}
+
+export default App;
